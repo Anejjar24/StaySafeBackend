567f8f3dfc77cc56748c16424b7307f2505d0513
ihssane <anejjarihssane@gmail.com>
2024-12-24 15:41:37 +0100

    entity location

---
+++ src/main/java/ma/ensaj/StaySafe/entity/Location.java
@@ -0,0 +1,26 @@
+package ma.ensaj.StaySafe.entity;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+public class Location {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Double latitude;
+    private Double longitude;
+    private LocalDateTime timestamp;
+    private Boolean isEmergency;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    private String message;
+    private LocationStatus status = LocationStatus.ACTIVE;
+}
+
---
+++ src/main/java/ma/ensaj/StaySafe/entity/LocationStatus.java
@@ -0,0 +1,11 @@
+package ma.ensaj.StaySafe.entity;
+
+public enum LocationStatus {
+    ACTIVE, RESOLVED, EXPIRED
+}
+/*
+
+ACTIVE : Indique que la localisation est actuellement active ou en cours d'utilisation.
+RESOLVED : Indique que la localisation a été traitée ou résolue (par exemple, une situation d'urgence a été prise en charge).
+EXPIRED : Indique que la localisation n'est plus valide ou qu'elle est arrivée à échéance (par exemple, une ancienne localisation dépassée).
+ */
