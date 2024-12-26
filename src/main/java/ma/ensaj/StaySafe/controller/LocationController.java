6b83e256b1b3135b44c461574a859d94134f6524
ihssane <anejjarihssane@gmail.com>
2024-12-26 14:31:06 +0100

    location de nouveau

--- src/main/java/ma/ensaj/StaySafe/controller/LocationController.java
+++ src/main/java/ma/ensaj/StaySafe/controller/LocationController.java
@@ -1,13 +1,20 @@
 package ma.ensaj.StaySafe.controller;
 
 
+import ma.ensaj.StaySafe.dto.LocationDTO;
 import ma.ensaj.StaySafe.entity.Location;
+import ma.ensaj.StaySafe.entity.LocationStatus;
+import ma.ensaj.StaySafe.entity.User;
+import ma.ensaj.StaySafe.repository.UserRepository;
 import ma.ensaj.StaySafe.service.LocationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/locations")
@@ -15,11 +22,37 @@ import java.util.List;
 public class LocationController {
     @Autowired
     private LocationService locationService;
+    @Autowired
+    private UserRepository userRepository;
 
-    @PostMapping
-    public ResponseEntity<Location> create(@RequestBody Location location) {
-        return ResponseEntity.ok(locationService.save(location));
+//    @PostMapping
+//    public ResponseEntity<Location> create(@RequestBody Location location) {
+//        return ResponseEntity.ok(locationService.save(location));
+//    }
+@PostMapping
+public ResponseEntity<Location> create(@RequestBody LocationDTO locationDTO) {
+    // Rechercher l'utilisateur par ID
+    Optional<User> userOptional = userRepository.findById(locationDTO.getUserId());
+    if (userOptional.isEmpty()) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
     }
+
+    User user = userOptional.get();
+
+    // Créer et associer la localisation
+    Location location = new Location();
+    location.setLatitude(locationDTO.getLatitude());
+    location.setLongitude(locationDTO.getLongitude());
+    location.setTimestamp(LocalDateTime.parse(locationDTO.getTimestamp()));
+    location.setIsEmergency(locationDTO.getIsEmergency());
+    location.setStatus(LocationStatus.valueOf(locationDTO.getStatus()));
+    location.setUser(user); // Associer l'utilisateur
+
+    Location savedLocation = locationService.save(location);
+
+    return ResponseEntity.status(HttpStatus.CREATED).body(savedLocation);
+}
+
 
     @GetMapping
     public ResponseEntity<List<Location>> getAll() {
--- src/main/java/ma/ensaj/StaySafe/dto/LocationDTO.java
+++ src/main/java/ma/ensaj/StaySafe/dto/LocationDTO.java
@@ -1,11 +1,12 @@
 package ma.ensaj.StaySafe.dto;
 
 import lombok.Data;
-
 @Data
 public class LocationDTO {
     private Double latitude;
     private Double longitude;
+    private String timestamp;
     private Boolean isEmergency;
-    private String message;
+    private String status;
+    private Long userId; // ID de l'utilisateur
 }
